791871cd1887ef13d8484b62ca5a28d409ee8ee5
Aseem Gaurav <cottonstartiet@gmail.com>
2024-12-04 14:07:01 +0530

    project setup issues

--- CodingPractice/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CodingPractice;
-using CodingPractice.Logger;
-
-BinarySearch bs = new();
-Log.Info(bs.Search(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 7));
-Log.Info(bs.Search(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 10));
